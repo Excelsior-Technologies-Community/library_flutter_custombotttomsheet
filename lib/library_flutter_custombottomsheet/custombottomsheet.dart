39701c0534f6eb43a82502661d10d062e13ec2e6
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-29 16:04:21 +0530

    ui updates in bottomsheet

--- lib/library_flutter_custombottomsheet/custombottomsheet.dart
+++ lib/library_flutter_custombottomsheet/custombottomsheet.dart
@@ -8,44 +8,103 @@ class CustombottomsheetNavigation extends StatefulWidget {
   final Color? backgroundColor;
   final double? elevation;
   final Color? selectedIconColor;
+  final Color? unselectedIconColor;
+  final bool showSelectedLabel;
+  final bool showUnselectedLabel;
+  final double selectedFontSize;
+  final double unselectedFontSize;
+  final Color? selectedLabelColor;
+  final Color? unselectedLabelColor;
+  final String? fontFamily;
+  final Function(int index)? onItemTap;
 
-  const CustombottomsheetNavigation({super.key, this.defaultIndex = 0,
+  const CustombottomsheetNavigation({
+    super.key,
+    this.defaultIndex = 0,
     required this.bottomsheetlength,
     required this.bottomsheetIcons,
     this.iconNames,
     this.backgroundColor,
     this.elevation,
     this.selectedIconColor,
+    this.unselectedIconColor,
+    this.showSelectedLabel = true,
+    this.showUnselectedLabel = true,
+    this.selectedFontSize = 15,
+    this.unselectedFontSize = 13,
+    this.selectedLabelColor,
+    this.unselectedLabelColor,
+    this.fontFamily,
+    this.onItemTap,
   });
 
   @override
-  State<CustombottomsheetNavigation> createState() => _CustombottomsheetNavigationState();
+  State<CustombottomsheetNavigation> createState() =>
+      _CustombottomsheetNavigationState();
 }
 
-class _CustombottomsheetNavigationState extends State<CustombottomsheetNavigation> {
-  int selectedIndex=0;
+class _CustombottomsheetNavigationState
+    extends State<CustombottomsheetNavigation> {
+  int selectedIndex = 0;
+
   @override
   void initState() {
-    // TODO: implement initState
-    selectedIndex= widget.defaultIndex;
+    selectedIndex = widget.defaultIndex;
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: selectedIndex,
       backgroundColor: widget.backgroundColor,
       elevation: widget.elevation,
-      currentIndex: selectedIndex,
-      selectedIconTheme: IconThemeData(color: widget.selectedIconColor),
-        onTap: (value) {
-          setState(() {
-            selectedIndex = value;
-          });
+      showSelectedLabels: widget.showSelectedLabel,
+      showUnselectedLabels: widget.showUnselectedLabel,
+      selectedFontSize: widget.selectedFontSize,
+      unselectedFontSize: widget.unselectedFontSize,
+      selectedItemColor: widget.selectedLabelColor,
+      unselectedItemColor: widget.unselectedLabelColor,
+
+      selectedIconTheme: IconThemeData(
+        color: widget.selectedIconColor,
+      ),
+
+      unselectedIconTheme: IconThemeData(
+        color: widget.unselectedIconColor,
+      ),
+
+      selectedLabelStyle: TextStyle(
+        fontFamily: widget.fontFamily,
+        fontSize: widget.selectedFontSize,
+        color: widget.selectedLabelColor,
+        fontWeight: FontWeight.w600,
+      ),
+
+      unselectedLabelStyle: TextStyle(
+        fontFamily: widget.fontFamily,
+        fontSize: widget.unselectedFontSize,
+        color: widget.unselectedLabelColor,
+      ),
+
+      onTap: (value) {
+        setState(() {
+          selectedIndex = value;
+        });
+      },
+
+      items: List.generate(
+        widget.bottomsheetlength,
+            (index) {
+          return BottomNavigationBarItem(
+            icon: Icon(
+              widget.bottomsheetIcons[index] ?? Icons.stop,
+            ),
+            label: widget.iconNames?[index] ?? "",
+          );
         },
-      items: List.generate(widget.bottomsheetlength, (index) {
-        return BottomNavigationBarItem(icon: Icon(widget.bottomsheetIcons[index] ?? Icons.stop), label: widget.iconNames?[index] ?? "");
-      },),
-      type: BottomNavigationBarType.fixed
+      ),
     );
   }
 }

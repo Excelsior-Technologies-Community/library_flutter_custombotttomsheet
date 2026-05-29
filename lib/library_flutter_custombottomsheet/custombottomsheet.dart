365d281816ef59f93652461362acd340de2ddd03
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-29 14:27:35 +0530

    inizile bottomsheet

---
+++ lib/library_flutter_custombottomsheet/custombottomsheet.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustombottomsheetNavigation extends StatelessWidget {
+  final int defaultIndex;
+  final int bottomsheetlength;
+  final List<IconData> bottomsheetIcons;
+
+  const CustombottomsheetNavigation({super.key, this.defaultIndex = 0,
+    required this.bottomsheetlength,
+    required this.bottomsheetIcons,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: defaultIndex,
+      items: List.generate(bottomsheetlength, (index) {
+        return BottomNavigationBarItem(icon: Icon(bottomsheetIcons as IconData));
+      },),
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+}
